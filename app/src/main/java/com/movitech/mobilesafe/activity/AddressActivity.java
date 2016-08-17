17be18a7fa7ccf45ad5ee3bc8f6edcc3e3282780
jennifer.zhang <912332606@qq.com>
2016-08-17 09:07:44 +0800

    监听EditText输入

--- app/src/main/java/com/movitech/mobilesafe/activity/AddressActivity.java
+++ app/src/main/java/com/movitech/mobilesafe/activity/AddressActivity.java
@@ -2,7 +2,9 @@ package com.movitech.mobilesafe.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,6 +24,24 @@ public class AddressActivity extends AppCompatActivity {
 
         et_number = (EditText) findViewById(R.id.et_number);
         tv_result = (TextView) findViewById(R.id.tv_result);
+
+        et_number.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                String address = AddressDao.getAddress(s.toString());
+                tv_result.setText(address);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void query(View view) {
